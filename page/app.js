4162afe35367dea2e83c55fd7996714e73838bfb
AlHassanHK <al.hassan.elkady@gmail.com>
2022-08-01 14:58:26 +0200

    add CORS

--- page/app.js
+++ page/app.js
@@ -2,8 +2,21 @@ const express = require("express");
 const registrationRoutes = require("./routes/registrationRoutes")
 const app = express();
 require("../mongo");
-const morgan = require("morgan");
 
+const morgan = require("morgan");
+pp.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  }); 
+   
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
