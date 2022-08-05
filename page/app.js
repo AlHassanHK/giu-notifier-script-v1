380b24616cfad15d2b143198956c8d055232dca5
AlHassanHK <al.hassan.elkady@gmail.com>
2022-08-05 20:31:52 +0200

    fix typo

--- page/app.js
+++ page/app.js
@@ -4,7 +4,7 @@ const app = express();
 require("../mongo");
 
 const morgan = require("morgan");
-pp.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
